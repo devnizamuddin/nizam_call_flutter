27aa10094a07717f0734d807471c5236817120ed
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-06-21 15:56:02 +0600

    code structurred

---
+++ lib/config/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  static const String AGORA_APP_ID = '26618f172fca4030b75e5b5bb1b082c9';
+  static const String AGORA_CHANNEL_ID = 'flutter_app';
+  static const String AGORA_TOKEN =
+      '007eJxTYPh42Ld0aV69+4Pvq24+mFJcULOrJ6m+Ibxk+2fpiytclrgpMBiZmRlapBmaG6UlJ5oYGBskmZummiaZJiUZJhlYGCVbvlg7KaUhkJFBVuA9AyMUgvjcDGk5pSUlqUXxiQUFDAwAN4klHw==';
+}
