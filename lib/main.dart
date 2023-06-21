89e5f0db225af01712a8012af512e13b2421436e
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-06-21 16:38:00 +0600

    project initialized with getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:nizam_call_flutter/ui/video_calll_view.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const VideoCallView(),
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: 'Application',
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
--- lib/utils/handle_permission.dart
+++
@@ -1,5 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-Future handlePermissionForVideoCall() async {
-  await [Permission.microphone, Permission.camera].request();
-}
